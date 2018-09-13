fc3f1bbce2ad1aeef849703a770b6c0cdb9ac29f
Rafael Blay <rblay@terra.com.br>
2018-09-13 16:48:44 -0700

    initial set up working

--- spring_boot_example/src/main/java/com/example/spring_boot_example/web/HelloWorldController.java
+++ spring_boot_example/src/main/java/com/example/spring_boot_example/web/HelloWorldController.java
@@ -4,12 +4,17 @@ package com.example.spring_boot_example.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class HelloWorldController {
 
+    @CrossOrigin()
     @RequestMapping("/hello")
     public String sayHello(@RequestParam(value = "name") String name){
-        return "Hello " + name + "!";
+        return "{\n" +
+            "    \"id\": 1,\n" +
+                    "    \"content\": \"Hello, World!\"\n" +
+                    "}";
     }
 }
